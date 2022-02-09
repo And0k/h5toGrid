c082f838e82637fdaacdfa514dbf751701cebd2d
Andrey Korzh <ao.korzh@gmail.com>
2022-02-09 02:36:38 +0200

    leo-editor tree for different datasets/coord. loading

--- scripts/copernicus_api_client.py
+++ scripts/copernicus_api_client.py
@@ -1,4 +1,8 @@
+#@+leo-ver=5-thin
+#@+node:3947220829000151.20211215072208.1: * @file copernicus_api_client.py
 #!/usr/bin/env python
+#@+others
+#@+node:3947220829000151.20211215192713.1: ** Declarations (copernicus_api_client.py)
 """
 Download data from Copernicus to NetCDF file - official motuclient configured to my needs with detecting last data date
 See also
@@ -33,7 +37,7 @@ import sys
 from pathlib import Path
 from datetime import datetime
 import pandas as pd
-import motuclient
+from motuclient import motuclient
 #from motuclient import main as retrieve, ERROR_CODE_EXIT
 # LIBRARIES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'motu_utils')
 # # Manage imports of project libraries
@@ -47,11 +51,12 @@ import motuclient
 # import motu_api
 
 
+#@-others
 if False:
     from motuclient import initLogger, load_options, motu_api
     import motu_api
 
-dir_save = r'd:\WorkData\BalticSea\_other_data\_model\Copernicus\Sal_bin=1month'
+dir_save = r'd:\WorkData\BalticSea\_other_data\_model\Copernicus\section_z'
 # file_nc_name = f'wind@ECMWF-ERA5_area({lat_st},{lon_st},{lat_en},{lon_en}).nc'
 # {file_nc_name}
 
@@ -115,7 +120,7 @@ if True:
                 log.debug(' . %s', stack.replace('\n', ''))
             log.debug('-' * 60)
 
-    path_config = Path('./cfg/coperinicus_api_client.ini').absolute()  # /scripts
+    path_config = Path('scripts/cfg/coperinicus_api_client.ini').absolute()  # /scripts
     if not path_config.is_file():
         l.error('Error %s - config not found!', path_config)
         sys.exit(1)
@@ -133,6 +138,9 @@ if True:
 
 
 print('ok')
+
+#@+<<data description>>
+#@+node:3947220829000151.20211215192837.1: ** <<data description>>
 """
 This Baltic Sea physical model product provides forecasts for the physical conditions in the Baltic Sea. The Baltic forecast is updated twice daily providing a new six days forecast. Three datasets are provided with hourly instantaneous values for sea level variations, ice concentration and thickness at the surface, and temperature, salinity and horizontal velocities for the 3D field. Additionally a dataset with 15 minutes (instantaneous) values are provided for the sea level variation and the horizontal surface currents. The product is produced by a Baltic Sea set up of the NEMOv4.0 ocean model. This product is provided at the models native grid with a resolution of 1 nautical mile in the horizontal, and up to 56 vertical depth levels. The area covers the Baltic Sea including the transition area towards the North Sea (i.e. the Danish Belts, the Kattegat and Skagerrak). The ocean model is offline coupled with Stokes drift data from the Baltic Wave forecast product (BALTICSEA_ANALYSISFORECAST_WAV_003_010).
 Classification
@@ -162,6 +170,13 @@ vo	Northward current	northward_sea_water_velocity	m s-1
 sla	Sea level elevation	sea_surface_height_above_sea_level	m
 siconc	Sea ice cover	sea_ice_area_fraction	1
 sithick	Sea ice thickness	sea_ice_thickness	m
+
+## BALTICSEA_REANALYSIS_PHY_003_011-TDS
+python -m motuclient --motu https://my.cmems-du.eu/motu-web/Motu --service-id BALTICSEA_REANALYSIS_PHY_003_011-TDS --product-id dataset-reanalysis-nemo-dailymeans --longitude-min 18.972 --longitude-max 19.235 --latitude-min 55.841 --latitude-max 55.929 --date-min "2014-12-01 12:00:00" --date-max "2020-12-31 12:00:00" --depth-min 66.5774 --depth-max 121.7625 --variable bottomT --variable so --variable sob --variable thetao --variable uo --variable vo --out-dir <OUTPUT_DIRECTORY> --out-name <OUTPUT_FILENAME> --user <USERNAME> --pwd <PASSWORD>
+
+python -m motuclient.motuclient --motu https://my.cmems-du.eu/motu-web/Motu --service-id BALTICSEA_REANALYSIS_BIO_003_012-TDS --product-id dataset-reanalysis-scobi-dailymeans --longitude-min 18.972 --longitude-max 19.235 --latitude-min 55.841 --latitude-max 55.929 --date-min "2014-12-01 12:00:00" --date-max "2020-12-31 12:00:00" --depth-min 66.5774 --depth-max 121.7625 --variable o2 --variable o2b --out-dir <OUTPUT_DIRECTORY> --out-name <OUTPUT_FILENAME> --user <USERNAME> --pwd <PASSWORD>
 """
-
-
+#@-<<data description>>
+#@@language python
+#@@tabwidth -4
+#@-leo
